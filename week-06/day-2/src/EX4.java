8ac4d2e05a0222ef32b9e002623bc3564c8160f7
JO <olsanska.jana@gmail.com>
2019-05-14 15:47:34 +0200

    streams

--- week-06/day-2/src/EX4.java
+++ week-06/day-2/src/EX4.java
@@ -5,7 +5,8 @@ import java.util.List;
 public class EX4 {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
-        IntSummaryStatistics stats = numbers.stream().filter(x -> x % 2 != 0)
+        IntSummaryStatistics stats = numbers.stream()
+                                            .filter(x -> x % 2 != 0)
                                             .mapToInt(x -> x)
                                             .summaryStatistics();
         System.out.println(stats.getAverage());
